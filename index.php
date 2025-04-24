e8ae93ed4075b137283ad19938bdfd4f59b5660f
ruta-emm <rutayisireemmanuel0780@gmail.com>
2025-04-24 12:59:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Emmanuel Rutayisire PHP</title>
+</head>
+<body>
+    <h1>Emmanuel Rutayisire PHP</h1>
+
+    <pre>
+    ***********
+    **       **
+    **
+    **
+    **
+    **       **
+    ***********
+    </pre>
+
+    <p>The SHA256 hash of "Emmanuel Rutayisire" is:</p>
+    <p>
+        <?php
+            echo hash('sha256', 'Emmanuel Rutayisire');
+        ?>
+    </p>
+
+    <p>
+        <a href="check.php">Click here to check the error setting</a><br>
+        <a href="fail.php">Click here to cause a traceback</a>
+    </p>
+</body>
+</html>
